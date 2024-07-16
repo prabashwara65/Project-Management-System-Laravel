87cb70e047faf2b78bf447536e5edde7aacd6bca
prabashwara65 <prabashwara65@gmail.com>
2024-07-16 20:00:22 +0530

    showing success message

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,19 +9,35 @@ class Project extends Model
 {
     use HasFactory;
 
-    public function tasks(){
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        
+        'image_path',
+        'name',
+        'description',
+        'status',
+        'due_date',
+        'created_by',
+        'updated_by',
+    ];
 
+    public function tasks()
+    {
         return $this->hasMany(Task::class);
     }
 
-   
-    public function createdBy(){
-
+    public function createdBy()
+    {
         return $this->belongsTo(User::class, 'created_by');
     }
 
-    public function updatedBy(){
-        
+    public function updatedBy()
+    {
         return $this->belongsTo(User::class, 'updated_by');
     }
 }
+
