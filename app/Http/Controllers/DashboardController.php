590c8d01e9d3d9c9f6163b7d9148f7ac4f0dbecf
prabashwara65 <prabashwara65@gmail.com>
2024-07-18 20:09:53 +0530

    DashboardController --> methods

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,17 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
+
+        $user = auth()->user();
+        $totalPendingTasks = Task::query()
+            ->where('status', 'pending')
+            ->count();
+        $myPendingTasks = Task::query()
+            ->where('status', 'pending')
+            ->where('assigned_user_id', $user->id)
+            ->count();
+
+
         return inertia('Dashboard');
     } 
 }
