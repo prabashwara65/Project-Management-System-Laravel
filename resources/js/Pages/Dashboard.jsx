6f9b78c094b9d6d9f5072b152c113935d1d552b7
prabashwara65 <prabashwara65@gmail.com>
2024-07-18 21:19:15 +0530

    commit

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { TASK_STATUS_CLASS_MAP, TASK_STATUS_TEXT_MAP } from "@/constants";
 import { Head, Link } from "@inertiajs/react";
 
+
 export default function Dashboard({
   auth,
   totalPendingTasks,
