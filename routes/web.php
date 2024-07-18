3daad1c5954136cd1226abd68e97837c4daa3655
prabashwara65 <prabashwara65@gmail.com>
2024-07-18 20:07:42 +0530

    DashboardController --> add functions

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TaskController;
