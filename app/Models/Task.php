f3d928d322bb21a2f4a67fd074e8990173f0c626
prabashwara65 <prabashwara65@gmail.com>
2024-07-18 18:58:16 +0530

    models.task add fillerble

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,24 +9,36 @@ class Task extends Model
 {
     use HasFactory;
 
-    public function project(){
+    protected $fillable = [
+        'name',
+        'description',
+        'image_path',
+        'status',
+        'priority',
+        'due_date',
+        'assigned_user_id',
+        'created_by',
+        'updated_by',
+        'project_id',
+    ];
 
-  
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
     }
 
-    public function assignedUser(){
-
-        return $this->belongsTo(User::class , 'assigned_user_id');
+    public function assignedUser()
+    {
+        return $this->belongsTo(User::class, 'assigned_user_id');
     }
 
-    public function createdBy(){
-
-        return $this->belongsTo(User::class , 'created_by');
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
     }
 
-    public function updatedBy(){
-
-        return $this->belongsTo(User::class , 'updated_by');
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
     }
-
 }
