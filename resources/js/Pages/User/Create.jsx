71d254658bc47f80835fd01aa72a0374e657dd28
prabashwara65 <prabashwara65@gmail.com>
2024-07-18 17:04:49 +0530

    user.index

--- resources/js/Pages/User/Create.jsx
+++ resources/js/Pages/User/Create.jsx
@@ -18,7 +18,7 @@ export default function Create({ auth }) {
   const onSubmit = (e) => {
     e.preventDefault();
 
-    post(route("project.store"));
+    post(route("user.store"));
   };
 
   return (
@@ -27,12 +27,12 @@ export default function Create({ auth }) {
       header={
         <div className="flex justify-between items-center">
           <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            Create new Project
+            Create new User
           </h2>
         </div>
       }
     >
-      <Head title="Projects" />
+      <Head title="Users" />
 
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -43,11 +43,11 @@ export default function Create({ auth }) {
             >
               <div>
                 <InputLabel
-                  htmlFor="project_image_path"
-                  value="Project Image"
+                  htmlFor="user_image_path"
+                  value="User Image"
                 />
                 <TextInput
-                  id="project_image_path"
+                  id="user_image_path"
                   type="file"
                   name="image"
                   className="mt-1 block w-full"
@@ -56,10 +56,10 @@ export default function Create({ auth }) {
                 <InputError message={errors.image} className="mt-2" />
               </div>
               <div className="mt-4">
-                <InputLabel htmlFor="project_name" value="Project Name" />
+                <InputLabel htmlFor="user_name" value="User Name" />
 
                 <TextInput
-                  id="project_name"
+                  id="user_name"
                   type="text"
                   name="name"
                   value={data.name}
@@ -72,12 +72,12 @@ export default function Create({ auth }) {
               </div>
               <div className="mt-4">
                 <InputLabel
-                  htmlFor="project_description"
-                  value="Project Description"
+                  htmlFor="user_description"
+                  value="User Description"
                 />
 
                 <TextAreaInput
-                  id="project_description"
+                  id="user_description"
                   name="description"
                   value={data.description}
                   className="mt-1 block w-full"
@@ -88,12 +88,12 @@ export default function Create({ auth }) {
               </div>
               <div className="mt-4">
                 <InputLabel
-                  htmlFor="project_due_date"
-                  value="Project Deadline"
+                  htmlFor="user_due_date"
+                  value="User Deadline"
                 />
 
                 <TextInput
-                  id="project_due_date"
+                  id="user_due_date"
                   type="date"
                   name="due_date"
                   value={data.due_date}
@@ -104,11 +104,11 @@ export default function Create({ auth }) {
                 <InputError message={errors.due_date} className="mt-2" />
               </div>
               <div className="mt-4">
-                <InputLabel htmlFor="project_status" value="Project Status" />
+                <InputLabel htmlFor="user_status" value="User Status" />
 
                 <SelectInput
                   name="status"
-                  id="project_status"
+                  id="user_status"
                   className="mt-1 block w-full"
                   onChange={(e) => setData("status", e.target.value)}
                 >
@@ -118,11 +118,11 @@ export default function Create({ auth }) {
                   <option value="completed">Completed</option>
                 </SelectInput>
 
-                <InputError message={errors.project_status} className="mt-2" />
+                <InputError message={errors.user_status} className="mt-2" />
               </div>
               <div className="mt-4 text-right">
                 <Link
-                  href={route("project.index")}
+                  href={route("user.index")}
                   className="bg-gray-100 py-1 px-3 text-gray-800 rounded shadow transition-all hover:bg-gray-200 mr-2"
                 >
                   Cancel
